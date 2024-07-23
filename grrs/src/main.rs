d2461c21f46f981a62865728fbe94d668fc71035
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-24 08:46:55 +0900

    grrs: accept required arguments

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // Example
+    //   $ cargo run --quiet -- some-pattern some-file
+    //   pattern: "some-pattern", path: "some-file"
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    println!("pattern: {:?}, path: {:?}", pattern, path);
 }
