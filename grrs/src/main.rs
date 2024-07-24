0b4fd47e00ce2075d305fd4dd2a8440e254de926
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-24 09:12:07 +0900

    grrs: structure args with struct

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,9 @@
+struct Cli {
+    pattern: String,
+    // PathBuf is like a String but for file system paths that work cross-platform.
+    path: std::path::PathBuf,
+}
+
 fn main() {
     // Example
     //   $ cargo run --quiet -- some-pattern some-file
@@ -5,5 +11,10 @@ fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
 
-    println!("pattern: {:?}, path: {:?}", pattern, path);
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
