0605ef550c850c16a1d849fdbb4e10500f37c406
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-25 15:52:06 +0900

    grrs: add test

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -24,18 +24,25 @@ fn main() -> Result<()> {
     // Cli::parse() is meant to be used in our main(); don't use it in other places.
     let args: Cli = Cli::parse();
 
-    // BufReader.lines() reads a file more efficiently than std::fs::read_to_string().
     let f: File =
         File::open(&args.path).with_context(|| format!("could not read file `{:?}`", args.path))?;
 
+    let _: Result<()> = find_matches(f, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+fn find_matches(f: File, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    // BufReader.lines() reads a file more efficiently than std::fs::read_to_string().
     let reader: BufReader<File> = BufReader::new(f);
 
     for line in reader.lines() {
         // With a question mark, Rust will internally expand the Result.
         let s = line?;
 
-        if s.contains(&args.pattern) {
-            println!("{}", s);
+        if s.contains(&pattern) {
+            // writeln!() returns an io::Result because writing can fail.
+            writeln!(writer, "{}", s)?;
         }
     }
 
