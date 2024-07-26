df1108ad3f6c350e6d824d95c1e9a1225d53832c
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-26 09:10:38 +0900

    hello: test stdout

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,10 +1,20 @@
 use assert_cmd::Command;
+use pretty_assertions::assert_eq;
 
 #[test]
 fn runs() {
     // Just call Result::unwrap because the binary should always be found.
     let mut cmd: Command = Command::cargo_bin("hello").unwrap();
 
-    // Verify the command succeeds
-    cmd.assert().success();
+    // Execute the command.
+    let output = cmd.output().expect("fail");
+
+    // Verify the command succeeds.
+    assert!(output.status.success());
+
+    // Convert the output of the program to UTF-8.
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+
+    // Verify the output is correct.
+    assert_eq!(stdout, "Hello, world!\n");
 }
