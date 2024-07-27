2ffc9732490f533c8509dfb9a5f959fa3c3a410c
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-27 11:59:17 +0900

    echor: first implementation

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,34 @@
+use clap::Arg;
+use clap::ArgAction;
+use clap::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Masatoshi N")
+        .about("Rust version of `echo`")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .help("Do not print newline"),
+        )
+        .get_matches();
+
+    // The type annotation is required because Iterator::collect can return many diffrent types.
+    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
+
+    let omit_newline = matches.get_flag("omit_newline");
+    let ending = if omit_newline { "" } else { "\n" };
+
+    print!("{}{}", text.join(" "), ending);
+
+    ()
 }
