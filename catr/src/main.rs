57c1306a1cdc1639782122c8494c857c1eaf7f97
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-28 16:02:10 +0900

    catr: print args

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+/// Concatenate FILE(s) to standard output.
+/// With no FILE, or when FILE is -, read standard input.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+
+    /// Number all output lines
+    #[arg(short = 'n', long, conflicts_with = "number_nonblank")]
+    number: bool,
+
+    /// Number nonempty output lines
+    #[arg(short = 'b', long)]
+    number_nonblank: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:#?}", args);
+
+    ()
 }
