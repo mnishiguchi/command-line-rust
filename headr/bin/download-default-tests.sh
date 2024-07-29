a569396e48c770e44052227e3181ace5be34ca57
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-07-29 20:20:12 +0900

    headr: download tests
    - https://github.com/kyclark/command-line-rust/tree/main/04_headr

---
+++ headr/bin/download-default-tests.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# Where this script is located
+# https://stackoverflow.com/a/246128/3837223
+this_name="$(basename "$0")"
+this_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Make a temporary directory
+td=$(mktemp -d)
+
+# Where the destination directory is located
+tests_dest="${this_dir}/../tests/"
+mkdir -p "${tests_dest}"
+
+# Download the example project to the temporary directory (sparsely)
+git clone --sparse --depth=1 --filter=blob:none \
+  https://github.com/kyclark/command-line-rust.git \
+  "${td}"
+
+# Copy necessary code
+(
+  cd "${td}" &&
+    git sparse-checkout add 04_headr &&
+    cd 04_headr &&
+    ./mk-outs.sh &&
+    mv -iv ./tests/* "${tests_dest}"
+)
+
+# Clean up the temporary directory
+rm -rf "${td}"
+
+# Install necessary crates
+cargo add anyhow
+cargo add clap --features derive
+cargo add --dev assert_cmd predicates pretty_assertions rand
+
+# Run test
+cargo test --quiet
