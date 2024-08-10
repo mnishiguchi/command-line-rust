2fc994bcd8e46f96ca4755cedd55309e9b443f7d
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-08-10 13:17:32 +0900

    wcr: get file info

--- wcr/src/main.rs
+++ wcr/src/main.rs
@@ -30,6 +30,14 @@ struct Args {
     chars: bool,
 }
 
+#[derive(Debug, PartialEq)]
+struct FileInfo {
+    line_count: usize,
+    word_count: usize,
+    byte_count: usize,
+    char_count: usize,
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
 
@@ -63,8 +71,9 @@ fn run(mut args: Args) -> Result<()> {
             Err(e) => {
                 eprintln!("{filename}: {e}")
             }
-            Ok(_) => {
-                println!("Opened {filename}")
+            Ok(filehandle) => {
+                let file_info = get_file_info(filehandle)?;
+                println!("{file_info:?}");
             }
         }
     }
@@ -85,5 +94,74 @@ fn open_input_source(filename: &str) -> Result<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn get_file_info(mut filehandle: impl BufRead) -> Result<FileInfo> {
+    // Initialize counters.
+    let mut line_count = 0;
+    let mut word_count = 0;
+    let mut byte_count = 0;
+    let mut char_count = 0;
+
+    // Create a mutable buffer to hold each line of text.
+    let mut line_buffer = String::new();
+
+    // Create an infinite loop for reading each line from the filehandle.
+    loop {
+        // BufRead::read_line preserves the line endings, as opposed to BufRead::lines removing the
+        // line endings.
+        let bytes_read = filehandle.read_line(&mut line_buffer)?;
+
+        // Break out of the loop when end of file has been reached.
+        if bytes_read == 0 {
+            break;
+        }
+
+        byte_count += bytes_read;
+        line_count += 1;
+        word_count += line_buffer.split_whitespace().count();
+        char_count += line_buffer.chars().count();
+
+        // Clear the line buffer for the next line of text.
+        line_buffer.clear();
+    }
+
+    Ok(FileInfo {
+        line_count,
+        word_count,
+        byte_count,
+        char_count,
+    })
+}
+
+// Unit tests
+//
+// The cfg(test) enables conditional compilation, so this module will be compiled only when
+// testing.
+#[cfg(test)]
+mod tests {
+    // Import from the parent module super (next above).
+    use super::*;
+
+    #[test]
+    fn test_get_file_info() {
+        // Fake a filehandle.
+        let filehandle =
+            std::io::Cursor::new("I don't want the world.\nI just want your half.\r\n");
+
+        let file_info = get_file_info(filehandle);
+        assert!(file_info.is_ok());
+
+        // This comparison required FileInfo to implement the PartialEq trait.
+        assert_eq!(
+            file_info.unwrap(),
+            FileInfo {
+                line_count: 2,
+                word_count: 10,
+                char_count: 48,
+                byte_count: 48,
+            }
+        );
     }
 }
