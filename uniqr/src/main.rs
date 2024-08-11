c67cc66430f5b1b77e156ee2abfd96b1f5ca7078
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-08-11 17:36:49 +0900

    uniqr: define args

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+
+/// Report or omit repeated lines
+#[derive(Debug, Parser, Clone)]
+#[command(author, version, about)]
+struct Args {
+    /// Input file
+    #[arg(value_name = "INPUT", default_value = "-")]
+    in_file: String,
+
+    /// Output file
+    #[arg(value_name = "OUTPUT")]
+    out_file: Option<String>,
+
+    /// Prefix lines by the number of occurrences
+    #[arg(short, long)]
+    count: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if let Err(e) = run(args.clone()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+
+    println!("{:?}", args);
+    Ok(())
+}
+
+fn run(_args: Args) -> Result<()> {
+    Ok(())
 }
