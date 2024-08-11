79923778226a5b5b1f24a172b7641ae17c5d14c2
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-08-11 18:31:10 +0900

    uniqr: open input source and read lines

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,5 +1,9 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 /// Report or omit repeated lines
 #[derive(Debug, Parser, Clone)]
@@ -21,15 +25,41 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    if let Err(e) = run(args.clone()) {
+    if let Err(e) = do_run(args) {
         eprintln!("{e}");
         std::process::exit(1);
     }
 
-    println!("{:?}", args);
     Ok(())
 }
 
-fn run(_args: Args) -> Result<()> {
+fn do_run(args: Args) -> Result<()> {
+    // Create an informative error message on failure.
+    let mut filehandle =
+        open_input_source(&args.in_file).map_err(|e| anyhow!("{}: {}", args.in_file, e))?;
+
+    // Create a new empty mutable string buffer to hold each line.
+    let mut line_buffer = String::new();
+
+    // Read lines of text from an input file or STDIN, preserving the line endings.
+    loop {
+        let bytes_read = filehandle.read_line(&mut line_buffer)?;
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        print!("{line_buffer}");
+
+        line_buffer.clear();
+    }
+
     Ok(())
+}
+
+fn open_input_source(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
