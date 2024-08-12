344aa6f8d9df690da930d83e248d3961ac677b2e
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-08-12 09:12:16 +0900

    uniqr: do uniq

--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -38,21 +38,42 @@ fn do_run(args: Args) -> Result<()> {
     let mut filehandle =
         open_input_source(&args.in_file).map_err(|e| anyhow!("{}: {}", args.in_file, e))?;
 
-    // Create a new empty mutable string buffer to hold each line.
-    let mut line_buffer = String::new();
+    let print_info_row = |n: u64, s: &str| {
+        // Print the output only when count is greater than 0.
+        if n > 0 {
+            if args.count {
+                print!("{:>4} {}", n, s);
+            } else {
+                print!("{}", s);
+            }
+        }
+    };
+
+    let mut current_line = String::new();
+    let mut previous_line = String::new();
+    let mut duplicate_count: u64 = 0;
 
     // Read lines of text from an input file or STDIN, preserving the line endings.
     loop {
-        let bytes_read = filehandle.read_line(&mut line_buffer)?;
+        let bytes_read = filehandle.read_line(&mut current_line)?;
 
         if bytes_read == 0 {
             break;
         }
 
-        print!("{line_buffer}");
+        let is_different_from_previous = current_line.trim_end() != previous_line.trim_end();
 
-        line_buffer.clear();
+        if is_different_from_previous {
+            print_info_row(duplicate_count, &previous_line);
+            previous_line = current_line.clone();
+            duplicate_count = 0;
+        }
+
+        duplicate_count += 1;
+        current_line.clear();
     }
+
+    print_info_row(duplicate_count, &previous_line);
 
     Ok(())
 }
