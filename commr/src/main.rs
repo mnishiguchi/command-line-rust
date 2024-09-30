150bc04e4b062e9633c0db67d432a930ac4f2244
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-10-01 08:33:36 +0900

    commr: validate and open input files

--- commr/src/main.rs
+++ commr/src/main.rs
@@ -1,4 +1,8 @@
 use clap::{ArgAction, Parser};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 /// compare two sorted files line by line
 #[derive(Debug, clap::Parser, Clone)]
@@ -51,5 +55,31 @@ fn main() {
 fn do_run(args: CliArguments) -> anyhow::Result<()> {
     println!("{:#?}", args);
 
+    let file1 = &args.file1;
+    let file2 = &args.file2;
+
+    // Prohibit that both the filenames being "-"
+    if file1 == "-" && file2 == "-" {
+        anyhow::bail!(r#"Both input files cannot be STDIN ("-")"#);
+    }
+
+    // Attempt to open the two input files
+    let _filehandle1 = open_input_file(file1)?;
+    let _filehandle2 = open_input_file(file2)?;
+    println!(r#"Opened "{file1}" and "{file2}""#);
+
     Ok(())
+}
+
+// Opening user-provided input source
+fn open_input_file(filename: &str) -> anyhow::Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => {
+            // Incorporate the filename into the error message
+            Ok(Box::new(BufReader::new(
+                File::open(filename).map_err(|e| anyhow::anyhow!("{filename}: {e}"))?,
+            )))
+        }
+    }
 }
